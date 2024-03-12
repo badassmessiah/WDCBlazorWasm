a42beb38ed7e71672f23c086e9ab85e23a705f6d
badassmessiah <lashaandguladze69@gmail.com>
2024-03-12 15:08:30 +0400

    Added Auth

--- BlazorWasmWdcApp/Program.cs
+++ BlazorWasmWdcApp/Program.cs
@@ -14,6 +14,18 @@ namespace BlazorWasmWdcApp
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             
 
+            var baseUrl = builder.Configuration.GetSection("MicrosoftGraph")["BaseUrl"];
+            var scopes = builder.Configuration.GetSection("MicrosoftGraph:Scopes").Get<List<string>>();
+            builder.Services.AddGraphClient(baseUrl, scopes);
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+				builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+				options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Calendars.ReadWrite");
+
+			});
+
             await builder.Build().RunAsync();
         }
 
